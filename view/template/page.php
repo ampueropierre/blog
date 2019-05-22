809c6097ed1e0a5f2067818649588e948c2ee36d
Ampuero Pierre <ampuero.pierre@gmail.com>
2019-05-22 18:07:56 +0200

    add html main in page and post

--- view/template/page.php
+++ view/template/page.php
@@ -12,14 +12,16 @@
   </header>
 
   <!-- Main Content -->
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
-        <?= $content ?>
-        <!-- Pager -->
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <?= $content ?>
+          <!-- Pager -->
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 
 <?php include 'include/_footer.php'; ?>
 <?php include 'include/_bottom.php'; ?>
--- view/template/post.php
+++ view/template/post.php
@@ -1,5 +1,4 @@
 <?php include 'include/_head.php'; ?>
-
   <!-- Page Header -->
   <header class="masthead d-flex align-items-center" style="background-image: url('public/img/home-bg.png')">
     <div class="container">
@@ -11,15 +10,18 @@
       </div>
     </div>
   </header>
+  
   <!-- Main Content -->
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
-        <?= $content ?>
-        <!-- Pager -->
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <?= $content ?>
+          <!-- Pager -->
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 
 <?php include 'include/_footer.php'; ?>
 <?php include 'include/_bottom.php'; ?>
