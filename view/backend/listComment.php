954553f076559461ca2421cbe861e9a13dd96a26
Ampuero Pierre <ampuero.pierre@gmail.com>
2019-05-23 13:41:17 +0200

    Change scss for listComment backend

--- view/backend/listComment.php
+++ view/backend/listComment.php
@@ -1,19 +1,12 @@
 <?php ob_start(); ?>
-<?php if (isset($_GET['success'])):?>
-<div class="alert alert-success" role="alert">
-  	Le commentaire a bien été ajouter
-</div>
-<?php endif; ?>
-<div>
-	<h3>Liste des commentaires en attente</h3>
-</div>
+<h3 class="text-center mb-4">Liste des commentaires en attente</h3>
+
 <table class="table mb-4">
 	<thead class="thead-light">
 		<tr>
 			<th scope="col">id</th>
 			<th scope="col">Le commentaire</th>
 			<th scope="col">Date</th>
-			<th>Statut</th>
 			<th scope="col">Action</th>
 		</tr>
 	</thead>
@@ -21,31 +14,30 @@
 		<?php foreach ($comments as $comment): ?>
 			<?php if ($comment->getStatus() == 0): ?>	
 			<tr>
-				<th><?= $comment->getId() ?></th>
-				<td>
-					<?= substr($comment->getComment(),0,40) ?>
-					<?php if(strlen($comment->getComment()) > 40): ?>
+				<td><?= $comment->getId() ?></td>
+				<td><?= substr($comment->getComment(),0,40) ?><?php if(strlen($comment->getComment()) > 40): ?>
 						...
 					<?php endif; ?>
 				</td>
 				<td><?= $comment->getCommentDate()->format('d/m/Y à H:i:s') ?></td>
-				<td><?= ($comment->getStatus() == 1) ? 'Valide' : 'Pas encore valide'?></td>
-				<td><a href="?action=updateComment&id=<?= $comment->getId() ?>" class="text-primary mr-2">Modifier</a><a href="?action=deleteComment&id=<?= $comment->getId() ?>" class="text-danger">Supprimer</a></td>
+				<td>
+					<a href="admin/comments/update/<?= $comment->getId() ?>" class="btn btn-outline-primary mr-2">Modifier</a>
+					<a href="admin/comments/delete/<?= $comment->getId() ?>" class="btn btn-outline-danger">Supprimer</a>
+				</td>
 			</tr>
 			<?php endif ?>
 		<?php endforeach; ?>
 	</tbody>
 </table>
-<div>
-	<h3>Liste des commentaires validés</h3>
-</div>
+
+<h3 class="text-center mb-4">Liste des commentaires validés</h3>
+
 <table class="table">
 	<thead class="thead-light">
 		<tr>
 			<th scope="col">id</th>
 			<th scope="col">Le commentaire</th>
 			<th scope="col">Date</th>
-			<th>Statut</th>
 			<th scope="col">Action</th>
 		</tr>
 	</thead>
@@ -53,7 +45,7 @@
 		<?php foreach ($comments as $comment): ?>
 			<?php if ($comment->getStatus() == 1): ?>
 			<tr>
-				<th><?= $comment->getId() ?></th>
+				<td><?= $comment->getId() ?></td>
 				<td>
 					<?= substr($comment->getComment(),0,40) ?>
 					<?php if(strlen($comment->getComment()) > 40): ?>
@@ -61,8 +53,10 @@
 					<?php endif; ?>
 				</td>
 				<td><?= $comment->getCommentDate()->format('d/m/Y à H:i:s') ?></td>
-				<td><?= ($comment->getStatus() == 1) ? 'Valide' : 'Pas encore valide'?></td>
-				<td><a href="?action=updateComment&id=<?= $comment->getId() ?>" class="text-primary mr-2">Modifier</a><a href="?action=deleteComment&id=<?= $comment->getId() ?>" class="text-danger">Supprimer</a></td>
+				<td>
+					<a href="admin/comments/update/<?= $comment->getId() ?>" class="btn btn-outline-primary mr-2">Modifier</a>
+					<a href="admin/comments/delete/<?= $comment->getId() ?>" class="btn btn-outline-danger">Supprimer</a>
+				</td>
 			</tr>
 			<?php endif ?>
 		<?php endforeach; ?>
