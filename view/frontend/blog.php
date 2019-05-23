dda2c84363d5dba51cbe3abd95b93cbd06c3102e
Ampuero Pierre <ampuero.pierre@gmail.com>
2019-05-23 10:37:33 +0200

    change scss

--- view/frontend/blog.php
+++ view/frontend/blog.php
@@ -1,27 +1,21 @@
-<?php
-ob_start();
-
-foreach ($posts as $post)
-{
-?>
+<?php ob_start();?>
+<?php foreach ($posts as $post): ?>
 <div class="post-preview">
 	<a href="posts/<?= $post->getId() ?>">
 		<h2 class="post-title">
 			<?= htmlspecialchars($post->getTitle()) ?>
 		</h2>
-		<p>
+		<h3 class="post-subtitle">
 			<?= htmlspecialchars($post->getChapo()) ?>
-		</p>
+		</h3>
 	</a>
 	<p class="post-meta">
 		Dernière modification le <?= $post->getDateModification()->format('d F Y à H:i:s') ?>
 	</p>
 </div>
 <hr>
+<?php endforeach ?>
 <?php
-}
-
 $content= ob_get_clean();
-
 require('view/template/page.php');
 ?>
