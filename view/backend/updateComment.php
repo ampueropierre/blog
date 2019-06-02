107901b1e7ce5739bda217fd5eda085a3de297eb
Ampuero Pierre <ampuero.pierre@gmail.com>
2019-06-03 01:35:24 +0200

    Redesign page updateUser and updateComment

--- view/backend/updateComment.php
+++ view/backend/updateComment.php
@@ -1,22 +1,25 @@
-<h3>Le commentaire</h3>
-
-<p>L'auteur : </p>
-<p><?= $comment->getAuthor()->getFirstname().' '.$comment->getAuthor()->getLastname()?></p>
-<p>Le commentaire :</p>
-<p><?= $comment->getContent() ?></p>
-
-<form action="" method="POST">
+<div class="update-comment">
 	<div class="form-group">
-		<label for="status">Statut</label>
-		<select class="form-control" id="status" name="status">
-			<?php for($i = 0; $i < 2; $i++):?>
-			<option value="<?= $i ?>" <?= ($i == $comment->getStatus()) ? 'selected' : '' ?>><?= ($i == 0) ? 'En attente de Validation' : 'Validé' ?></option>
-			<?php endfor; ?>
-		</select>
-		<?php if (isset($errors) && in_array($commentValidator::STATUS_INVALID, $errors)):?>
-		<span class="text-danger"><?= $commentValidator::STATUS_INVALID ?></span>
-		<?php endif; ?>
+		<span class="meta">L'auteur</span>
+		<p><?= $comment->getAuthor()->getFirstname().' '.$comment->getAuthor()->getLastname()?></p>
+	</div>		
+	<div class="form-group">
+		<span class="meta">Le commentaire</span>
+		<p><?= $comment->getContent() ?></p>
 	</div>
+	<form action="" method="POST">
+		<div class="form-group">
+			<label class="meta" for="status">Statut</label>
+			<select class="form-control" id="status" name="status">
+				<?php for($i = 0; $i < 2; $i++):?>
+					<option value="<?= $i ?>" <?= ($i == $comment->getStatus()) ? 'selected' : '' ?>><?= ($i == 0) ? 'En attente de Validation' : 'Validé' ?></option>
+				<?php endfor; ?>
+			</select>
+			<?php if (isset($errors) && in_array($commentValidator::STATUS_INVALID, $errors)):?>
+			<span class="text-danger"><?= $commentValidator::STATUS_INVALID ?></span>
+		<?php endif; ?>
+		</div>
 	
-	<button type="submit" class="btn btn-primary" name="update">Modifier</button>
-</form>
+		<button type="submit" class="btn btn-primary" name="update">Modifier</button>
+	</form>
+</div>
--- view/backend/updateUser.php
+++ view/backend/updateUser.php
@@ -3,42 +3,37 @@
 	Le Rôle a bien été modifié
 </div>
 <?php endif ?>
-<?php
-var_dump($_POST);
-?>
-<div class="row mb-3">
-	<div class="col-md-2">Prénom :</div>
-	<div class="col-md-10"><?= $user->getFirstname() ?></div>
-</div>
-<div class="row my-3">
-	<div class="col-md-2">Nom :</div>
-	<div class="col-md-10"><?= $user->getLastname() ?></div>
-</div>
-<div class="row my-3">
-	<div class="col-md-2">Mail :</div>
-	<div class="col-md-10"><?= $user->getMail() ?></div>
-</div>
-
-<form action="" method="POST">
-	<div class="form-group row my-3">
-		<label for="status" class="col-md-2">Rôle :</label>
-		<div class="col-md-10">
+<div class="update-user">
+	<div class="form-group">
+		<span class="meta">Prénom</span>
+		<p><?= $user->getFirstname() ?></p>
+	</div>
+	<div class="form-group">
+		<span class="meta">Nom</span>
+		<p><?= $user->getLastname() ?></p>
+	</div>
+	<div class="form-group">
+		<span class="meta">Mail</span>
+		<p><?= $user->getMail() ?></p>
+	</div>
+	<form action="" method="POST">
+		<div class="form-group">
+			<label for="status" class="meta">Rôle</label>
 			<select class="form-control" id="role" name="role">
 				<?php foreach ($roles as $role): ?>
-					<option value="<?= $role['id'] ?>"
-					<?php if ($user->getRolesId() == $role['id']): ?>
-						selected
-					<?php endif; ?>
-					>
-					<?= $role['name'] ?>	
-					</option>
+				<option value="<?= $role['id'] ?>"
+				<?php if ($user->getRolesId() == $role['id']): ?>
+					selected
+				<?php endif; ?>
+				>
+				<?= $role['name'] ?>	
+				</option>
 				<?php endforeach; ?>
 			</select>
+			<?php if (isset($errors) && in_array($userValidator::ROLE_NOTEXIST, $errors)):?>
+			<span class="text-danger"><?= $userValidator::ROLE_NOTEXIST ?></span>
+			<?php endif; ?>
 		</div>
-		<?php if (isset($errors) && in_array($userValidator::ROLE_NOTEXIST, $errors)):?>
-		<span class="text-danger"><?= $userValidator::ROLE_NOTEXIST ?></span>
-		<?php endif; ?>
-	</div>
-	
-	<button type="submit" class="btn btn-primary" name="update">Modifier</button>
-</form>
+		<button type="submit" class="btn btn-primary" name="update">Modifier</button>
+	</form>
+</div>
